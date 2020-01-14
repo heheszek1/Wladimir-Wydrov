707e2bb28be709ba87f1b4cad615ed486acb125d
heheszek1 <59340864+heheszek1@users.noreply.github.com>
2020-01-14 10:52:31 +0100

    Delete serviceworker.js

--- serviceworker.js
+++
@@ -1,8 +0,0 @@
-function spawnNotification(theBody,theIcon,theTitle) {
-  var options = {
-    body: theBody,
-    icon: theIcon
-  }
-  var n = new Notification(theTitle,options);
-}
-setTimeout(spawnNotification(), 3000);
